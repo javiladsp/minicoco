68a7155253ab7cc24ddc239fbc37895d1966a7f8
joseavilasg <joseavilasg.dev@gmail.com>
2024-09-11 10:15:24 -0500

    Complement script with different functionalities

--- script.py
+++ script.py
@@ -9,8 +9,10 @@ from alive_progress import alive_bar
 from pybboxes import BoundingBox
 
 import requests
+import shutil
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 parser = argparse.ArgumentParser()
 
@@ -39,9 +41,25 @@ parser.add_argument("-f", "--format", type=str,
                     choices=["coco", "yolo"],
                     default="coco")
 
+parser.add_argument("-anylabel", "--anylabel", type=bool,
+                    help="Create a label file for each image for the AnyLabel tool.",
+                    default=True)
+
+parser.add_argument("-d", "--destination", type=str,
+                    help="Destination folder.",
+                    default="./")
+
+parser.add_argument("-w", "--workers", type=int,
+                    help="Number of workers.",
+                    default=4)
+
+parser.add_argument("-l", "--local", type=str,
+                    help="Local path to the images.",
+                    default="")
+
 args = parser.parse_args()
 
-ROOT_PATH = "dataset"
+ROOT_PATH = os.path.join(args.destination, "dataset")
 TRAIN_IMAGES_PATH = f"{ROOT_PATH}/train/images"
 TRAIN_LABELS_PATH = f"{ROOT_PATH}/train/labels"
 VAL_IMAGES_PATH = f"{ROOT_PATH}/val/images"
@@ -64,6 +82,9 @@ def process_subset(annotation_file, num_images, subset_name):
     imgShuffled = sample(imgOriginals, len(imgOriginals))
 
     def myImages(images: list, num: int) -> list:
+        if num == -1:
+            return images
+
         return images[:num]
 
     def cocoJson(images: list) -> dict:
@@ -98,7 +119,7 @@ def process_subset(annotation_file, num_images, subset_name):
         dictCOCO = {k: coco.getCatIds(k)[0] for k in catNms}
         dictCOCOSorted = dict(sorted(dictCOCO.items(), key=lambda x: x[1]))
 
-        IdCategories = list(range(1, len(catNms)+1))
+        IdCategories = list(range(1, len(catNms) + 1))
         categories = dict(zip(list(dictCOCOSorted), IdCategories))
 
         arrayIds = np.array([k["id"] for k in images])
@@ -107,32 +128,51 @@ def process_subset(annotation_file, num_images, subset_name):
 
         yolo_annotations = []
 
-        for ann in anns:
-            image = next(img for img in images if img["id"] == ann["image_id"])
-            img_width = image["width"]
-            img_height = image["height"]
-            image_file_name = image["file_name"]
-            file_name = os.path.splitext(image_file_name)[0]
+        image_dict = {img["id"]: img for img in images}
 
-            # COCO bbox format: [x, y, width, height]
-            x, y, width, height = ann["bbox"]
+        with alive_bar(len(anns), title="Formatting annotations...") as bar:
+            for i, ann in enumerate(anns):
+                image = image_dict[ann["image_id"]]
+                img_width = image["width"]
+                img_height = image["height"]
+                image_file_name = image["file_name"]
+                file_name = os.path.splitext(image_file_name)[0]
 
-            coco_bbox = BoundingBox.from_coco(x, y, width, height, image_size=(img_width, img_height))
-            x_center, y_center, width, height = coco_bbox.to_yolo().raw_values
+                # COCO bbox format: [x, y, width, height]
+                x, y, coco_width, coco_height = ann["bbox"]
 
-            yolo_annotations.append({
-                "image_id": ann["image_id"],
-                "file_name": file_name,
-                "category_id": catIds.index(ann["category_id"]),
-                "bbox": {
-                    "x_center": x_center,
-                    "y_center": y_center,
-                    "width": width,
-                    "height": height
-                }
-            })
+                if any(dim is None or dim == 0 for dim in (coco_width, coco_height)):
+                    continue
 
-        cats = [{'id': int(value), 'name': key}
+                coco_bbox = BoundingBox.from_coco(x, y, coco_width, coco_height, image_size=(img_width, img_height))
+                x_center, y_center, width, height = coco_bbox.to_yolo(return_values=True)
+
+                yolo_annotations.append({
+                    "image_id": ann["image_id"],
+                    "file_name": file_name,
+                    "category_id": catIds.index(ann["category_id"]),
+                    "bbox": {
+                        "yolo": {
+                            "x_center": x_center,
+                            "y_center": y_center,
+                            "width": width,
+                            "height": height
+                        },
+                        "coco": {
+                            "x": x,
+                            "y": y,
+                            "width": coco_width,
+                            "height": coco_height
+                        }
+                    }
+                })
+
+                # Update the progress bar less frequently
+                # if i % 100 == 0:
+                bar()
+
+        # TODO: Check id categories
+        cats = [{'id': int(value) - 1, 'name': key}
                 for key, value in categories.items()]
         dataset = {}
         dataset["images"] = images
@@ -150,7 +190,7 @@ def process_subset(annotation_file, num_images, subset_name):
             for annotation in dataset["annotations"]:
                 file_name = annotation["file_name"]
                 category_id = annotation["category_id"]
-                bbox = annotation["bbox"]
+                bbox = annotation["bbox"]["yolo"]
                 with open(f"{ROOT_PATH}/{subset_name}/labels/{file_name}.txt", "a") as file:
                     file.write(f"{category_id} {bbox['x_center']} {bbox['y_center']} {bbox['width']} {bbox['height']}\n")
                 bar()
@@ -171,6 +211,95 @@ def process_subset(annotation_file, num_images, subset_name):
             file.write(f"nc: {len(dataset['categories'])}\n")
             file.write(f"names: {catNms}")
 
+    def createAnyLabelJson(dataset: dict) -> None:
+        annotations_by_image = {}
+
+        category_dict = {cat["id"]: cat["name"] for cat in dataset["categories"]}
+        image_dict = {img["id"]: img for img in dataset["images"]}
+
+        with alive_bar(len(dataset["annotations"]), title="Formatting annotations for AnyLabel...") as bar:
+            for i, annotation in enumerate(dataset["annotations"]):
+                image_id = annotation["image_id"]
+                file_name = annotation["file_name"]
+                category_id = annotation["category_id"]
+                bbox = annotation["bbox"]
+                category_name = category_dict[category_id]
+                image = image_dict[image_id]
+                image_w, image_h = image["width"], image["height"]
+
+                x = bbox["coco"]["x"]
+                y = bbox["coco"]["y"]
+                width = bbox["coco"]["width"]
+                height = bbox["coco"]["height"]
+
+                points = [
+                    [x, y],
+                    [x + width, y],
+                    [x + width, y + height],
+                    [x, y + height]
+                ]
+
+                shape = {
+                    "label": category_name,
+                    "score": None,
+                    "points": points,
+                    "group_id": category_id,
+                    "description": "",
+                    "difficult": False,
+                    "shape_type": "rectangle",
+                    "flags": {},
+                    "attributes": {}
+                }
+
+                if image_id not in annotations_by_image:
+                    annotations_by_image[image_id] = {
+                        "version": "2.3.6",
+                        "flags": {},
+                        "shapes": [],
+                        "imagePath": file_name + ".jpg",
+                        "imageData": None,
+                        "imageHeight": image_h,
+                        "imageWidth": image_w
+                    }
+
+                annotations_by_image[image_id]["shapes"].append(shape)
+
+                bar()
+
+        def save_json_file(json_data, json_file_path):
+            with open(json_file_path, "w") as json_file:
+                json.dump(json_data, json_file, indent=4)
+
+        with alive_bar(len(annotations_by_image), title=f'Creating labels for AnyLabel tool of the {subset_name} set:') as bar:
+            with ThreadPoolExecutor(max_workers=args.workers) as executor:
+                futures = [executor.submit(save_json_file, json_data, os.path.join(ROOT_PATH, subset_name, "images", f"{os.path.splitext(json_data['imagePath'])[0]}.json")) for json_data in annotations_by_image.values()]
+                for future in as_completed(futures):
+                    future.result()  # Ensure any exceptions are raised
+                    bar()
+
+    def copy_image(image, local_path, images_path):
+        src_path = os.path.join(local_path, image['file_name'])
+        dest_path = os.path.join(images_path, image['file_name'])
+        shutil.copy2(src_path, dest_path)
+
+    def copyImages(images: list, local_path: str, title: str) -> None:
+        images_path = f"{ROOT_PATH}/{subset_name}/images"
+        os.makedirs(images_path, exist_ok=True)
+
+        with alive_bar(len(images), title=title) as bar:
+            with ThreadPoolExecutor(max_workers=args.workers) as executor:
+                futures = [executor.submit(copy_image, image, local_path, images_path) for image in images]
+                for future in as_completed(futures):
+                    future.result()  # Ensure any exceptions are raised
+                    bar()
+
+    def download_image(session, image, images_path):
+        if not os.path.isfile(f"{images_path}/{image['file_name']}"):
+            img_data = session.get(image['coco_url']).content
+            with open(images_path + '/' + image['file_name'], 'wb') as handler:
+                handler.write(img_data)
+        return image['file_name']
+
     def downloadImages(images: list, title: str) -> None:
         session = requests.Session()
         retry = Retry(connect=3, backoff_factor=0.5)
@@ -179,18 +308,22 @@ def process_subset(annotation_file, num_images, subset_name):
         session.mount('https://', adapter)
 
         images_path = f"{ROOT_PATH}/{subset_name}/images"
+        os.makedirs(images_path, exist_ok=True)
 
         with alive_bar(len(images), title=title) as bar:
-            for image in images:
-                if not os.path.isfile(f"{images_path}/{image['file_name']}"):
-                    img_data = session.get(image['coco_url']).content
-                    with open(images_path + '/' + image['file_name'], 'wb') as handler:
-                        handler.write(img_data)
-                bar()
+            with ThreadPoolExecutor(max_workers=args.workers) as executor:
+                futures = [executor.submit(download_image, session, image, images_path) for image in images]
+                for future in as_completed(futures):
+                    future.result()  # Ensure any exceptions are raised
+                    bar()
 
     images = myImages(imgShuffled, num_images)
 
-    downloadImages(images, title=f'Downloading images of the {subset_name} set:')
+    if args.local != "":
+        local = os.path.join(args.local, "dataset", subset_name, "images")
+        copyImages(images, local, title=f"Copying images of the {subset_name} set:")
+    else:
+        downloadImages(images, title=f'Downloading images of the {subset_name} set:')
 
     if args.format == "coco":
         dataset = cocoJson(images)
@@ -198,9 +331,13 @@ def process_subset(annotation_file, num_images, subset_name):
     elif args.format == "yolo":
         dataset = yoloJson(images)
         createJson(dataset)
-        createYOLOLabels(dataset)
         createClasesFile(dataset)
         createDataYAML(dataset)
+
+    if args.anylabel:
+        createAnyLabelJson(dataset)
+    else:
+        createYOLOLabels(dataset)
 
 
 process_subset(args.train_annotation_file, args.training, "train")
